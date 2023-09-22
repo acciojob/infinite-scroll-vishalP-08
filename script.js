fdd86b38dddc3d2ca3e8ace70e6c9996791d9f9a
Vishal.AI <132605163+vishalP-08@users.noreply.github.com>
2023-09-22 19:10:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
-//your code here!
+document.addEventListener('DOMContentLoaded', () => {
+  const list = document.getElementById('infi-list');
+  let isLoading = false; // Variable to track if new items are being loaded
 
+  // Function to add a specified number of list items
+  const addListItems = (count) => {
+    for (let i = 0; i < count; i++) {
+      const listItem = document.createElement('li');
+      listItem.textContent = `List Item ${list.children.length + 1}`;
+      list.appendChild(listItem);
+    }
+  };
+
+  // Add 10 list items by default
+  addListItems(10);
+
+  // Function to handle scroll and add more items when reaching the end
+  const handleScroll = () => {
+    if (!isLoading && list.scrollTop + list.clientHeight >= list.scrollHeight - 10) {
+      isLoading = true; // Set loading state to prevent multiple calls
+
+      // Simulate loading delay (you can replace this with an API call)
+      setTimeout(() => {
+        // Add 2 more list items
+        addListItems(2);
+        isLoading = false; // Reset loading state
+      }, 1000); // Simulate a delay of 1 second (adjust as needed)
+    }
+  };
+
+  // Listen for scroll events on the list
+  list.addEventListener('scroll', handleScroll);
+});
